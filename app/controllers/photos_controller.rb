aae0bb41e27968f613312ae771849e2e05cbc7aa
Ahmad Jamal <akj824@gmail.com>
2015-06-18 23:25:09 +0000

    Further work on image upload feature for places.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,18 +1,14 @@
 class PhotosController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user! 
 	def create
 		@place = Place.find(params[:place_id])
 		@place.photos.create(photo_params)
-		redirect_to place_photos_path(@place)
-	end
-
-	
-	def index
+		redirect_to place_path(@place)
 	end
 
 	private
 
 	def photo_params
-		params.require(:photo) .permit(:caption)
+		params.require(:photo).permit(:caption, :picture)
 	end
 end
