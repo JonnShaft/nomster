72fb784534414cdfc436aec0c72dd6c8f9f59000
Ahmad Jamal <akj824@gmail.com>
2015-07-06 19:50:17 +0000

    Built model test for humanized ratings.

--- test/factories.rb
+++ test/factories.rb
@@ -1,3 +1,24 @@
-FactoryGirl.define do |variable|
-	
+FactoryGirl.define do
+
+	factory :user do
+		sequence :email do |n|
+			"ajamal#{n}@gmail.com"
+		end
+		password "akjakjakj321321"
+		password_confirmation "akjakjakj321321"
+	end
+
+	factory :place do
+		name "Test Bar 1"
+		description "Beer."
+		address "248 Palm Trace Ave., Las Vegas, NV 89148"
+		association :user
+	end
+
+	factory :comment do
+		message "Test, test, test."
+		rating "3_stars"
+		association :user
+		association :place
+	end
 end
