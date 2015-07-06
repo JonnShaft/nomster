3c9d1187a40291922de84b39f810e5fc0cb019ee
Ahmad Jamal <akj824@gmail.com>
2015-07-06 23:05:39 +0000

    Built controller test for comments controller.

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,13 +1,21 @@
-#require 'test_helper'
+require 'test_helper'
 
-#class CommentsControllerTest < ActionController::TestCase
- #  test "add comment" do
-  #  user = FactoryGirl.create(:user)
-	#sign_in user
+class CommentsControllerTest < ActionController::TestCase
+   test "add comment" do
+    user = FactoryGirl.create(:user)
+	sign_in user
 
-	#place = FactoryGirl.create(:place)
-	#comment = FactoryGirl.create(:comment)
-	#post :create, :place_id => place.id
-	#assert_response :success
-   #end
-#end
+	place = FactoryGirl.create(:place)
+	
+	assert_difference 'Comment.count' do
+			post :create, :place_id => place.id, :comment => {
+				:rating => '3_stars',
+				:message => 'test, test, test'
+			}
+	end
+
+	assert_equal 1, place.comments.count
+	assert_redirected_to place_path(place)
+   end
+
+end
